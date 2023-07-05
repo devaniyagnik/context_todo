5e65b3ae1103935bc9be968bafd1c7e675af14f3
yagnik <yagnikdevani00@gmail.com>
2023-07-05 16:25:19 +0530

    first all commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Signup from "./components/Singup";
+import Nopage from "./components/Nopage";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Edit from "./components/Edit";
+import { ThemeProvider, mycontext } from "./context/ThemeContext";
+
+import { useContext,useEffect } from "react";
+
 
 function App() {
+  const { isLoggedIn,setLocaldata,localdata} = useContext(mycontext);
+
+  useEffect(() => {
+    setLocaldata(JSON.parse(localStorage.getItem("data")) || []);
+  }, []);
+  
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <BrowserRouter>
+    
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isLoggedIn ? (
+                <Dashboard  />
+              ) : (
+                <Signup  />
+              )
+            }
+          />
+          <Route
+            path="/singup"
+            element={<Signup  />}
+          />
+
+          <Route path="login" element={<Login />} />
+          <Route
+            path="dashboard"
+            element={
+              <Dashboard  />
+            }
+          />
+          <Route
+            path="edit"
+            element={<Edit  />}
+          />
+          <Route path="*" element={<Nopage />} />
+        </Routes>
+        
+      </BrowserRouter>
+    </>
   );
 }
 
